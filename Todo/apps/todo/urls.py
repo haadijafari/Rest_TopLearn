59348416bcd9b09c290bc96977253e575c062941
Hadi Jafari <haadijafari2003@gmail.com>
2024-07-18 05:47:57 +0330

    [ADD] Class Base APIs

--- Todo/apps/todo/urls.py
+++ Todo/apps/todo/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.all_todos, name='all_todos'),
     path('<int:todo_id>', views.todo_detail, name='todo_detail'),
+    path('cbv', views.TodoListApiView.as_view(), name='all_todos_cbv'),
+    path('cbv/<int:todo_id>', views.TodoDetailApiView.as_view(), name='todo_detail_cbv'),
 ]
--- Todo/apps/todo/views.py
+++ Todo/apps/todo/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Todo
 from .serializers import TodoSerializer
 
 
+# region function base apis
 @api_view(['GET', 'POST'])
 def all_todos(request: Request):
     if request.method == 'GET':
@@ -41,3 +43,44 @@ def todo_detail(request: Request, todo_id):
     elif request.method == 'DELETE':
         todo.delete()
         return Response(None, status.HTTP_204_NO_CONTENT)
+
+
+# endregion
+
+
+# region class base apis
+class TodoListApiView(APIView):
+    def get(self, request: Request):
+        todos = Todo.objects.order_by('priority').all()
+        todo_serializer = TodoSerializer(todos, many=True)
+        return Response(todo_serializer.data, status.HTTP_200_OK)
+
+    def post(self, request: Response):
+        todo_serializer = TodoSerializer(data=request.data)
+        if todo_serializer.is_valid():
+            todo_serializer.save()
+            return Response(todo_serializer.data, status.HTTP_201_CREATED)
+        else:
+            return Response(None, status.HTTP_400_BAD_REQUEST)
+
+
+class TodoDetailApiView(APIView):
+    def get(self, request: Request, todo_id: int):
+        todo = get_object_or_404(Todo, pk=todo_id)
+        todo_serializer = TodoSerializer(todo, many=False)
+        return Response(todo_serializer.data, status.HTTP_200_OK)
+
+    def put(self, request: Request, todo_id: int):
+        todo = get_object_or_404(Todo, pk=todo_id)
+        todo_serializer = TodoSerializer(todo, data=request.data)
+        if todo_serializer.is_valid():
+            todo_serializer.save()
+            return Response(todo_serializer.data, status.HTTP_202_ACCEPTED)
+        else:
+            return Response(None, status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request: Request, todo_id: int):
+        todo = get_object_or_404(Todo, pk=todo_id)
+        todo.delete()
+        return Response(None, status.HTTP_204_NO_CONTENT)
+# endregion
